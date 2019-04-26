fb43446de39587334f73721caa0da4fd17737d9c
zelanxiao <xiaozelan@outlook.com>
2019-04-26 16:43:49 -0400

    Sprint React Practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// class App extends Component {
+//     state = {
+//         isLoading: true,
+//         groups: []
+//     };
+//
+//     async componentDidMount() {
+//         const response = await fetch('/api/groups');
+//         const body = await response.json();
+//         this.setState({ groups: body, isLoading: false });
+//     }
+//
+//     render() {
+//         const {groups, isLoading} = this.state;
+//
+//         if (isLoading) {
+//             return <p>Loading...</p>;
+//         }
+//
+//         return (
+//             <div className="App">
+//                 <header className="App-header">
+//                     <img src={logo} className="App-logo" alt="logo" />
+//                     <div className="App-intro">
+//                         <h2>JUG List</h2>
+//                         {groups.map(group =>
+//                             <div key={group.id}>
+//                                 {group.name}
+//                             </div>
+//                         )}
+//                     </div>
+//                 </header>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GroupList from './GroupList';
+import GroupEdit from './GroupEdit';
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path='/' exact={true} component={Home}/>
+                    <Route path='/groups' exact={true} component={GroupList}/>
+                    <Route path='/groups/:id' component={GroupEdit}/>
+                </Switch>
+            </Router>
+        )
+    }
 }
 
 export default App;
